8e263f6f94829610240da80c731682849f2101f4
Rodrigo Donizete Santana de Pádua <rodrigo.dsp@gmail.com>
2025-02-14 05:28:27 -0300

    Removing unused imports

--- weatherapp/src/main/java/com/codewithjava21/weatherapp/WeatherappApplication.java
+++ weatherapp/src/main/java/com/codewithjava21/weatherapp/WeatherappApplication.java
@@ -1,11 +1,9 @@
 package com.codewithjava21.weatherapp;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
